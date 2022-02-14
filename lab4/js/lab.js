1968b2f47da1d2b6a5dd801993638ebeb58e52af
YoBoyOboy <89765137+YoBoyOboy@users.noreply.github.com>
2022-02-14 00:54:58 -0800

    lab5

---
+++ lab4/js/lab.js
@@ -0,0 +1,24 @@
+// Description. (use period)
+// @link URL
+// @file This files defines the MyClass MyClass
+// @author Omar Lopez.
+// @since x.x.x
+//  Print the board before each turn
+    //  Loop through each row
+      //  Loop through each space
+        //  Print X, 0, or space
+        // Print a | between
+      //Print a newline at the end
+      //Print a horizontal line between
+// everyday tasks
+  //checking gas level in car
+    //checking expiration dates on food
+      //turn lights off before leaving the room
+        //pay subscriptions
+Creating a video animation:
+1) create a story
+2) sketch out characters and scenes
+3) finalize characters and scenes
+4) create scripts
+5) animate characters and scenes
+6) edit and finalize animations
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+  *Author: Omar
+  *Created 02.13.2022
+  * (c) Copyright by Omar
+**/
+
+//define variables
+var make = "Honda";
+var model = "Civic";
+var color = "Silver";
+var year = "2018";
+
+//calculate age
+age = 2022 - year;
+
+//outputs
+document.writeln("Make: " + make +"<br>");
+document.writeln("Model: " + model +"<br>");
+document.writeln("Color: " + color +"<br>");
+document.writeln("Year " + year +"<br>");
+document.writeln("Age " + age +"<br>");
