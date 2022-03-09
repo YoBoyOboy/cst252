e3cd2377a4003ab18256d6afe0c8d3f01812136a
YoBoyOboy <89765137+YoBoyOboy@users.noreply.github.com>
2022-03-08 23:34:22 -0800

    lab

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,29 +4,7 @@
 * License: Public Domain
 */
 
-function sortUsername() {
-  var userName = window.prompt("Give me your name *finger gun*");
-  console.log("userName =", userName);
-
-  var nameArray = userName.split('');
-  console.log("nameArraySort =", nameArray);
-
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort =", nameArraySort);
-
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSorted);
-
-  return nameSorted;
-}
-
-var headEl = document.getElementById("headshot");
-headEl.addEventListener("click", function(){
-    var name = prompt("Your name, sir?");
-    var title = document.getElementById('name');
-    title.innerHTML = name;
-});
-
-
-document.writeln("Heres your new name: ",
-  sortUsername(), "</br>")
+var bioEl = $(".biography")
+$(".biography").append("<button id=my-button>I dont know");
+$("#my-button").click(function(){alert("Alert!");})
+$("#my-button").click(function(){$(".assignments").toggleClass("green");})
